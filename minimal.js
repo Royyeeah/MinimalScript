4143f5368508b9a48f0e0c9f6b77a423ff0dfb53
Royyeeah <93230670+Royyeeah@users.noreply.github.com>
2021-10-26 20:04:08 -0400

    Update minimal.js

--- minimal.js
+++ minimal.js
@@ -1,5 +1,75 @@
-var console.log = cl;
+//library: 
+
+
+//popups:
 var alert = a;
-var parseInt = ps;
+
 var prompt = p;
-var Promise = p;
+
+//declaration or assurance that one will do a particular thing or that a particular thing will happen.
+var Promise = P;
+
+//integers:
+var parseInt = ps;
+
+
+//console:
+var console.log = cl;
+var console.clear = cc;
+var console.error = ce;
+
+
+
+
+// examples: 
+
+
+// (normal: calulator):
+
+var n1 = p(ps("enter first number");
+var n2 = p(ps("enter second number");
+
+sum = n1+n2;
+
+a("the answer to "+n1+" + "+n2+" is "+sum);
+
+
+
+// MinimalScript maximum basic: calulator:
+
+a((p(ps())+(p(ps()));
+
+   // vs plain js max basic:
+   
+   alert((prompt(parseInt())+(prompt(parseInt());
+                            
+
+                              
+//basic login:
+   
+var name;
+                              
+name = p(ps("Hey, What's your name?"));
+
+a("Well, nice to meet you, "+name+"!");
+                              
+                         
+                            
+                              
+                         
+                              
+                              
+                              
+                            
+                              
+                            
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
